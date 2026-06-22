ee39cfdc3b3895d46741f41a189e30bd58dc436a
Sohaib Kamash <sohaibkmsh@gmail.com>
2026-06-22 12:47:12 +0200

    Fix tonic-build compile function names

--- rk-cli/build.rs
+++ rk-cli/build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_proto("../proto/torrent.proto")?;
+    tonic_build::compile_protos("../proto/torrent.proto")?;
     Ok(())
 }
